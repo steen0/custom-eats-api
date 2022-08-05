ce0f8f2ad041ff96bd91f5cfb5fb5b4fe69b0077
tsteen <tsteen0731@gmail.com>
2022-08-05 17:11:42 -0400

    initial migrations for custom user model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,10 +7,15 @@ class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         email='test@example.com'
         password='testpass123'
-        user = get_user_model.objects.create_user(
+        user = get_user_model().objects.create_user(
             email=email,
             password=password,
         )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        email ='test@example.com'
+        password='testpass123'
+        user = get_user_model().objects.create_user
