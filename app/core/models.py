9abcd958c635f719c9611a2f853d35dd2942c854
tsteen <tsteen0731@gmail.com>
2022-08-05 12:55:10 -0400

    initial custom user model w/ simple tests

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
 # Create your models here.
-class User(models.Model):
-    pass
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
