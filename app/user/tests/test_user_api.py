16bcb5fde018034459c57938d9fee682970628c2
tsteen <tsteen0731@gmail.com>
2022-08-08 17:59:23 -0400

    created user app and registered in settings. wrote initial user:create tests

---
+++ app/user/tests/test_user_api.py
@@ -0,0 +1,33 @@
+"""
+Tests for the user API
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+
+CREATE_USER_URL = reverse('user:create')
+
+
+def create_user(**params):
+    return get_user_model().objects.create_user(**params)
+
+
+class PublicUserAPITests(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_user_success(self):
+        """Tests creating a user is successful"""
+        payload = {
+            'email': 'test@example.com',
+            'password': 'testpass123'
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(email=payload['email'])
